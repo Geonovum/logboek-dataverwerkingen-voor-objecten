d2659e013da69dd17a80be39b10b336a6791c291
Niels Hoffmann <n.hoffmann@geonovum.nl>
2025-06-16 16:57:02 +0200

    init extensie specificatie

---
+++ logboek_objecten_extensie/js/config.js
@@ -0,0 +1,40 @@
+let respecConfig = {
+  useLogo: true,
+  useLabel: true,
+  maxTocLevel: 3,
+  license: "cc-by",
+  specStatus: "WV",
+  specType: "PR",
+  pubDomain: "logboek",
+  shortName: "logboek-extensie-template",
+  publishDate: "2025-03-31",
+  publishVersion: "0.9",
+  prevVersion: [],
+
+  authors: [
+        {
+            name: "Niels Hoffmann (Geonovum)"
+        },
+        {
+            name: "Frank Terpstra (Geonovum)"
+        }
+  ],
+  editors: [
+        {
+            name: "Niels Hoffmann (Geonovum)"
+        },
+        {
+            name: "Frank Terpstra (Geonovum)"
+        },
+    ],
+  github: "https://github.com/Logius-standaarden/logboek-extensie-template",
+
+
+  // Create PDF and link to file in header (optional):
+  alternateFormats: [
+      {
+          label: "pdf",
+          uri: "template.pdf",
+      },
+  ],
+};
